b20e525c2e954c5efa737698812066108233588b
Thandeka93 <noluthandombali4@gmail.com>
2023-08-11 11:20:51 +0200

    get user

--- greet.js
+++ greet.js
@@ -1,35 +1,26 @@
 export default function Greet() {
   let greetCounter = 0;
   let users = [];
-  let usernameTrimmed = "";
   let greetMsg = "";
   let errorMsg = "";
-  let firstLetter, restOfLetters;
   const lettersOnlyRegex = /^[a-zA-Z]+$/;
   const namesCountMap = {};
 
   function peopleCounter(username) {
-    usernameTrimmed = username.trim();
-    if (lettersOnlyRegex.test(usernameTrimmed)) {
-      if (!users.includes(usernameTrimmed.toLowerCase())) {
-        greetCounter++;
-        users.push(usernameTrimmed.toLowerCase());
-      }
+    const usernameTrimmed = username.trim();
+    if (lettersOnlyRegex.test(usernameTrimmed) && !users.includes(usernameTrimmed.toLowerCase())) {
+      greetCounter++;
+      users.push(usernameTrimmed.toLowerCase());
     }
   }
-  function peopleGreeted() {
-    return greetCounter;
-  }
-  function getGreetedUsers() {
-    return users;
-  }
+
   function greetUserWithLanguage(language, username) {
     if (username !== undefined) {
-      usernameTrimmed = username.trim();
+      const usernameTrimmed = username.trim();
       if (lettersOnlyRegex.test(usernameTrimmed)) {
-        let arrName = usernameTrimmed.toLowerCase().split("");
-        [firstLetter, ...restOfLetters] = arrName;
-        let capitalizeName = firstLetter.toUpperCase() + restOfLetters.join("");
+        const arrName = usernameTrimmed.toLowerCase().split("");
+        const [firstLetter, ...restOfLetters] = arrName;
+        const capitalizeName = firstLetter.toUpperCase() + restOfLetters.join("");
         if (namesCountMap[username] === undefined) {
           namesCountMap[username] = 1;
         } else {
@@ -42,51 +33,40 @@ export default function Greet() {
         } else if (language === "Spanish") {
           greetMsg = `Hola ${capitalizeName}`;
         }
-      } else {
-        return "";
       }
     }
   }
 
-  function userGreetedIn() {
-    return greetMsg;
-  }
-  function getNamesCountMap() {
-    return namesCountMap;
-  }
   function displayErrorMsg(username, language) {
     if (username !== undefined) {
-      username === "" ? (errorMsg = "Please enter your name") : "";
-
-      language === undefined ? (errorMsg = "Please select a language") : "";
-
-      username === "" && language === undefined
-        ? (errorMsg = "Please select a language and enter your name") : "";
-
-      username !== "" && language !== undefined ? (errorMsg = "") : "";
+      if (username === "") {
+        errorMsg = "Please enter your name";
+      } else if (language === undefined) {
+        errorMsg = "Please select a language";
+      } else if (username === "" && language === undefined) {
+        errorMsg = "Please select a language and enter your name";
+      } else if (username !== "" && language !== undefined) {
+        errorMsg = "";
+      }
     }
-    
-  }
-  function currentErrorMsg() {
-    return errorMsg;
   }
 
   function resetCounter() {
     greetMsg = "";
     errorMsg = "";
     users = [];
-    return (greetCounter = 0);
+    greetCounter = 0;
   }
 
   return {
     peopleCounter,
-    peopleGreeted,
     greetUserWithLanguage,
     displayErrorMsg,
     resetCounter,
-    userGreetedIn,
-    currentErrorMsg,
-    getGreetedUsers,
-    getNamesCountMap,
+    userGreetedIn: () => greetMsg,
+    currentErrorMsg: () => errorMsg,
+    peopleGreeted: () => greetCounter,
+    getGreetedUsers: () => users,
+    getNamesCountMap: () => namesCountMap,
   };
 }
